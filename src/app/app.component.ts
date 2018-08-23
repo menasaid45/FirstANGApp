8f46b22acf4fabc409bdd528c1158764852304ec
Mina Said <menasaid45@gmail.com>
2018-08-23 14:41:27 +0200

    First commit with bootstrap and ngx-bootstrap packages

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'msn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'msn';
+}
